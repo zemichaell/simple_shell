246356cc5a73644f463a1236744069c11872fef9
zemichaell <zemichael2014@gmail.com>
2022-06-16 03:53:30 -0700

    simple shell

---
+++ shell.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main - simple shell
+ * @ac: Argument counter
+ * @av: Argument values
+ * @env: envaroment variables
+ *
+ * Return: 0
+ */
+
+int main(int ac, char **av, char **env)
+{
+	/* char *shell_pharse; */
+	command_t **cmd_list = NULL;/* Command List */
+	char *path = NULL;
+	/* history_t **history = malloc(sizeof(history_t)); */
+
+	path = find_path(env);
+	ac++;
+	/* shell_pharse = isatty(STDIN_FILENO) ? "> " : NULL; */
+	while (1)
+	{
+		cmd_list = _prompt(av[0], av[1]); /* get commands from cmd_line */
+		if (cmd_list)
+		{
+			if (_fork(av[0], *cmd_list, path, env))
+				error_handler(av[0], 102);
+		}
+		else
+			error_handler(av[0], 103);
+	}
+	return (0);
+}
